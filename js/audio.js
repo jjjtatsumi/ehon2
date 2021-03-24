8bde1a8edc03058bd112f740084b1bca7ea7e958
jjjtatsumi <77433447+jjjtatsumi@users.noreply.github.com>
2021-03-24 16:55:07 +0900

    set slider

--- js/audio.js
+++ js/audio.js
@@ -9,3 +9,13 @@ playButton.addEventListener('click', () => {
 stopButton.addEventListener('click', () => {
   audio.pause()
 })
+
+/*audio slidber */
+const slider_volume = document.getElementById("volume");
+const audioElement = document.querySelector("audio");
+
+var volume = document.getElementById('volume');
+
+slider_volume.addEventListener("input", e => {
+  audioElement.volume = slider_volume.value;
+});
